e804c41796fcdb6a2bcd992c49df26ec05af9d60
The Luciferous <shivamaima619@gmail.com>
2019-09-12 19:06:44 +0530

    Adding mobile responsiveness

--- src/Components/CollectionGridItem/CollectionGridItem.js
+++ src/Components/CollectionGridItem/CollectionGridItem.js
@@ -4,13 +4,18 @@ import "./CollectionGridItem.scss";
 import AddToList from "../AddToList/AddToList";
 
 const CollectionGridItem = ({ item, listItems, addItem }) => {
-  const { title, overview, backdrop_path, name } = item;
+  const { title, overview, backdrop_path, poster_path,name } = item;
   return (
     <div className="grid-item">
       <img
         src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop_path}`}
         alt="movie"
         className="grid-image"
+      />
+       <img
+        src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${poster_path}`}
+        alt="movie"
+        className="grid-image grid-mobile"
       />
       <div className={`${title ? "grid-movie-text" : "grid-tv-text"}`}>
         <h1 className="grid-name">{title}</h1>
