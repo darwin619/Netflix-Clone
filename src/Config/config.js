a57357169644d23b4de0f44785fcfad2a3589f71
The Luciferous <shivamaima619@gmail.com>
2019-09-15 17:05:09 +0530

    Added Redirects File to Connect React Router With Netlify

--- src/Config/config.js
+++ src/Config/config.js
@@ -13,13 +13,13 @@ const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 const BACKDROP_SIZE = "w1280";
 
 // w92, w154, w185, w342, w500, w780, original
-const GRID_IMAGE_SIZE = "w780";
+const GRID_IMAGE_SIZE = "w500";
 
 // w92, w154, w185, w342, w500, w780, original
-const POSTER_SIZE = "w780";
+const POSTER_SIZE = "w500";
 
 // w92, w154, w185, w342, w500, w780, original
-const PROFILE_SIZE = "w154";
+const PROFILE_SIZE = "w92";
 
 const TRAILER_LINK = "https://www.youtube.com/watch?v=";
 
