ac2b6cebef90ccf523c28705bc0c701d22dbdef6
Asifur Rahaman <asifurrahaman754@gmail.com>
2022-08-15 00:38:05 +0600

    added index.js file in component and pages folder to reduce imports line

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+import ListOverview from "./ListOverview/ListOverview";
+import Header from "./Header/Header";
+import SearchPage from "./SearchPage/SearchPage";
+
+export { ListOverview, Header, SearchPage };
---
+++ src/Pages/index.js
@@ -0,0 +1,8 @@
+import Movies from "./Movies/Movies";
+import TvShow from "./TVShows/TvShow";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import MovieItemPage from "./MovieItemPage/MovieItemPage";
+import TVShowItemPage from "./TVShowItemPage/TVShowItemPage";
+
+export { Movies, TvShow, SignIn, SignUp, MovieItemPage, TVShowItemPage };
